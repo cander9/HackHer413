2fc072211210bbebf0d2dfe4646d046aba580bfe
peachylauren <49697430+peachylauren@users.noreply.github.com>
2020-02-09 07:24:41 -0500

    Update Readability.java

--- Readability.java
+++ Readability.java
@@ -15,6 +15,14 @@ public class Readability {
         return(readReport);
     }
 
+    public String getReadabilityReport(String text){
+        String score = getReadabilityScore(text);
+        String report = "This is your readability score based on Flesch's Readability index: " + score;
+        report += "\nScores of 90 and above are readable at a 3rd grade level, while scores " +
+                "below 40 are at a college reading level.";
+        return(report);
+    }
+
     private double getASW(String text) {
         String[] words = text.split("\\W+");
         int totWordSyll = 0;
